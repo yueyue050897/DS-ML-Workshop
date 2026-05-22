41f2b6e4180f3829268cdbcbb67e28c864e91c59
yueyue050897 <jeerawadee.sun@gmail.com>
2026-05-22 15:32:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ st.write("##### Day 1: การจัดการข้อมูลพื้น
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
+elif st.button("การทำความสะอาดข้อมูล"):
+    st.switch_page("pages/cleaning_bytoon.py")
