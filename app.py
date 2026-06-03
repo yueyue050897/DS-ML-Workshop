082269d80a89a2b801348c6b1fc0d747f7e0011a
yueyue050897 <jeerawadee.sun@gmail.com>
2026-06-03 15:27:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ elif st.button("💰 การทำความสะอาดข้อมู�
     st.switch_page("pages/clean_bytoon.py")
 elif st.button("💰 แปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
-elif st.button("💰 แปลงข้อมูลA"):
-    st.switch_page("pages/clean_customers.py")
+elif st.button("💰 การวิเคราะห์ข้อมูลเชิงสำรวจ"):
+    st.switch_page("pages/EDA_app.py")
