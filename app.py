c5fe0e2ff6d2d8c0d41b68ab999cbb6e1082ce09
yueyue050897 <jeerawadee.sun@gmail.com>
2026-06-04 11:37:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,5 @@ elif st.button("💰 แปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
 elif st.button("💰 การวิเคราะห์ข้อมูลเชิงสำรวจ"):
     st.switch_page("pages/EDA_app.py")
+elif st.button("💰 การพยากรณ์ยอดขายแบบง่าย"):
+    st.switch_page("pages/sale_predict.py")
